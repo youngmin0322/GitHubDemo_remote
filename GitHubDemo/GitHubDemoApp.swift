259675df1a86bff9cc3dc058d43fd8e22d7a9bb4
youngmin <choyoungmin0322@gmail.com>
2023-05-21 19:17:49 +0900

    Initial Commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by 조영민 on 2023/05/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
